a78c8b2780fff0a1778a2452d29e5a9b85943ce9
Senarathne S M A D <amiladevin@gmail.com>
2022-02-26 20:36:05 +0530

    Factory

--- Object-oriented Programming in JavaScript/OOP JS Practicals/Index.js
+++ Object-oriented Programming in JavaScript/OOP JS Practicals/Index.js
@@ -1,15 +1,15 @@
-const circle = {
-    radius: 1,
-    location:{
-        x:1,
-        y:1
-    },
-    draw: function(){
-        console.log('draw');
-    }
+//Factorty Function
+function createCircle(radius){
+    return {
+        radius,
+        draw: function(){
+            console.log('draw');
+        }
+    
+    };
+}
 
-};
 
-//access circle object members
-
+//call createCircle funtion
+const circle = createCircle(1);
 circle.draw();
