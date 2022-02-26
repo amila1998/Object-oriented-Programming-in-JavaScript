828a7bbafe0aafe0d764dc5bdc49ff0597d24173
Senarathne S M A D <amiladevin@gmail.com>
2022-02-26 19:37:35 +0530

    Creating an object

--- Object-oriented Programming in JavaScript/OOP JS Practicals/Index.js
+++ Object-oriented Programming in JavaScript/OOP JS Practicals/Index.js
@@ -1,1 +1,15 @@
-console.log("Hello World")
+const circle = {
+    radius: 1,
+    location:{
+        x:1,
+        y:1
+    },
+    draw: function(){
+        console.log('draw');
+    }
+
+};
+
+//access circle object members
+
+circle.draw();
