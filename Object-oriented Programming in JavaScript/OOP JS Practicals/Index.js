67cfc97d5f829e8f8544a87fd154f6eac1a20cd8
Senarathne S M A D <amiladevin@gmail.com>
2022-02-27 00:16:10 +0530

    Enumerating Properties

--- Object-oriented Programming in JavaScript/OOP JS Practicals/Index.js
+++ Object-oriented Programming in JavaScript/OOP JS Practicals/Index.js
@@ -26,12 +26,36 @@ function Circle(radius){
 //calling  Constructor Function
 const circle2 = new Circle(10); //object 2
 
+for (let key in circle){
+    //console.log(key);
+    //console.log(key, circle[key]); //get the value of these properties
+    
+//get only the properties and not the methods.
+    if (typeof circle[key] !== 'function')
+        console.log(key, circle[key]);
+
+}
+
+//To get all the keys and an objects.
+const keys = Object.keys(circle);
+console.log(keys);
+
+
+//If an object has given property
+if ('radius' in circle)
+    console.log('Circle has a radius.')
+
+
+
+
 //Adding Properties
 //circle.location = {x:1};
-
+/*
 //another way
 const propertyName = 'location'
 circle['location'] = {x:1};
 
 //delete Properties
-delete circle['location'];
+delete circle['location'];*/
+
+
