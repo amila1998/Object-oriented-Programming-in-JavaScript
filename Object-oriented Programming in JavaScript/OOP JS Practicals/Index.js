08f321eb8d6ceb5fb3a99aef547228c260e061c4
Senarathne S M A D <amiladevin@gmail.com>
2022-02-27 01:37:58 +0530

    Abstraction- Getters and Setters

--- Object-oriented Programming in JavaScript/OOP JS Practicals/Index.js
+++ Object-oriented Programming in JavaScript/OOP JS Practicals/Index.js
@@ -1,61 +1,43 @@
 
- //Factorty Function
-function createCircle(radius){
-    return {
-        radius,
-        draw: function(){
-            console.log('draw');
-        }
-    
-    };
-}
-
-
-//call createCircle funtion
-const circle = createCircle(1); //object 1
 
 //Constructor Function
-function Circle(radius){
-    console.log('this', this) 
-    this.radius=radius,
-    this.draw= function(){
+function Circle(radius){ //parent function
+    
+    this.radius=radius;
+
+    let defaultLocation = {x:0, y:0};
+
+    /*
+    //getter
+    this.getDefultLocation = function(){
+        return defaultLocation;
+    };
+    */
+
+    this.draw= function(){    
         console.log('draw');
-    }
+    };
+
+   
+    Object.defineProperty(this, 'defaultLocation',{
+        //getter
+        get: function(){
+            return defaultLocation;
+        },
+         //Setter
+        set: function(value){
+            if(!value.x ||value.y)
+                throw new Error('Invalid Location')
+            defaultLocation = value;
+        }
+    });
 
 }
 //calling  Constructor Function
-const circle2 = new Circle(10); //object 2
-
-for (let key in circle){
-    //console.log(key);
-    //console.log(key, circle[key]); //get the value of these properties
-    
-//get only the properties and not the methods.
-    if (typeof circle[key] !== 'function')
-        console.log(key, circle[key]);
-
-}
-
-//To get all the keys and an objects.
-const keys = Object.keys(circle);
-console.log(keys);
+const circle = new Circle(10);
+circle.defaultLocation = 1; //this is for generate for an error
+circle.draw();
 
 
-//If an object has given property
-if ('radius' in circle)
-    console.log('Circle has a radius.')
-
-
-
-
-//Adding Properties
-//circle.location = {x:1};
-/*
-//another way
-const propertyName = 'location'
-circle['location'] = {x:1};
-
-//delete Properties
-delete circle['location'];*/
 
 
